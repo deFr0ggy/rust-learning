8a4f3d7ad31aa510e0a25b3f58bedf265b7ec641
Kamran Saifullah - Frog Man <16836050+deFr0ggy@users.noreply.github.com>
2024-01-06 20:47:05 +0300

    Set

--- target/debug/deps/challenge1-42e48e6c6768474c.d
+++
@@ -1,5 +0,0 @@
-/Users/frogman/Documents/GitHub/rust-learning/Challenges/challenge1/target/debug/deps/challenge1-42e48e6c6768474c: src/main.rs
-
-/Users/frogman/Documents/GitHub/rust-learning/Challenges/challenge1/target/debug/deps/challenge1-42e48e6c6768474c.d: src/main.rs
-
-src/main.rs:
